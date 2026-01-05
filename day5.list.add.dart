8f8764c33121090aa756cc6873138965f312c303
Joy <bijoyhasan1612@gmail.com>
2026-01-05 11:22:09 +0600

    Add files via upload

---
+++ day5.list.add.dart
@@ -0,0 +1,10 @@
+void main(){
+  var numbers = [1,2,3,4,5];
+  print(numbers);
+
+  numbers.add(6);
+  print(numbers);
+
+   numbers.addAll([7,8,9,10]);
+   print(numbers);
+}
---
+++ day5.list.dart
@@ -0,0 +1,10 @@
+void main (){
+ const city = ["Dhaka","Rajshahi","Gazipur","Chittagong"];
+ print(city); //using const to fix the array list
+
+ city.add("Cox's Bazar");
+ print(city);
+ city.add("Khulna");
+ print(city);
+ 
+}
