49fac7a981a0f18c259050bed889bf7b2f68a8c2
Joy <bijoyhasan1612@gmail.com>
2026-01-06 21:25:26 +0600

    Add files via upload

---
+++ day6.map.add.key.value.dart
@@ -0,0 +1,13 @@
+void main(){
+  var Student = {
+    'name': 'Joy',
+    'Id' : 267,
+    'Dept' : 'SWE',
+    'City' : 'Dhaka'
+  };
+  print(Student);
+
+  print("Adding Key-Value Pair:");
+  Student['Country'] = 'Bangladesh';
+  print(Student);
+}
---
+++ day6.map.constructor.dart
@@ -0,0 +1,14 @@
+void main (){
+  var student = new Map();
+
+  student['Name'] = 'Nahid';
+  student ['Age'] = 23;
+  student ['Dept'] = 'Software Engineering';
+  student ['Id'] = 474;
+
+  print(student);
+
+  student ['City'] = 'Cumilla';
+  print(student);
+
+}
