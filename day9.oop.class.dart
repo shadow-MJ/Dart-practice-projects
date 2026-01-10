cb16d33c18f2944f60ea4346950fc53e948e2564
Joy <bijoyhasan1612@gmail.com>
2026-01-10 23:02:19 +0600

    Add files via upload

---
+++ day9.oop.class.dart
@@ -0,0 +1,12 @@
+import 'day9.oop.class2.dart';
+
+void main (){
+
+var obj = new name();
+obj.addTwoNumbers(10,20);
+obj.addThreeNumbers(1, 2, 3);
+
+print('My name is: ${obj.myName}');
+print('The list is: ${obj.Alphabet}');
+print('2nd index element: ${obj.Alphabet[1]}');
+}
---
+++ day9.oop.class2.dart
@@ -0,0 +1,14 @@
+class name {
+
+  var myName = 'Abdullahi Muktadi Joy';
+  var Alphabet = ['a', 'b', 'c'];
+
+
+  addTwoNumbers(var a, var b) {
+    print(a + b);
+  }
+
+  addThreeNumbers(var x, var y, var z){
+    print(x+y+z);
+  }
+}
